10db18fa3af3eec681576143c5771cacd6e31fa0
vikaskumar9634160495-boop <vikaskumar9634160495@gmail.com>
2026-03-05 13:53:59 +0000

    vikas-imp

---
+++ 02-basic.js
@@ -0,0 +1,22 @@
+// const name = "vikas"
+// const age = 20
+
+// console.log(`my name is ${name} and my age is ${age}`);
+
+// useabe- charAt = find charecter place
+// charCodeAt = find charecter number
+// codePointAt = find charecter decimal value
+// concat = add the new charecter to old charecter
+// constructor = only charecter place
+// includes = chack the charecter (true/false)
+// indexOf = find charecter place
+// isWellFormed = only show (true)
+// lastIndexOf = find charecter place numbar for backside
+// length = calculate the length
+// localeCompare = show only 1
+// match = show index, input, groups,['your type word', 0, undefined ]
+
+
+const gamename = new String('vikas')
+
+console.log(gamename.padEnd())
